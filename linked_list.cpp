53f3538ac206b4049e543a9c8fb2eea14e34e6b4
Aditya <icgadi99@gmail.com>
2020-10-26 15:32:33 +0530

    detecting loop in sll using hashmap

--- linked_list.cpp
+++ linked_list.cpp
@@ -757,7 +757,59 @@
 // } runs fine. Another method is to use a fast and a slow pointer such that fast moves 2 times farther from slow pointer
 // in short, *fast = *fast->next->next & *slow = *slow->next;
 
-// Practise : Count number of occurences of a given key in a singly linked list
+// // Practise : Count number of occurences of a given key in a singly linked list
+// #include<bits/stdc++.h>
+// #define null NULL
+// using namespace std;
+// class Node{
+//     public:
+//     int data;
+//     Node* next;
+// };
+// void pushAtEnd(Node** head, int data){
+//     Node* newNode = new Node();
+//     newNode->data = data;
+//     newNode->next = null;
+//     if(*head == null){
+//         *head = newNode;
+//         return;
+//     }
+//     Node* temp = *head;
+//     while(temp->next != null) temp = temp->next;
+//     temp->next = newNode;
+// }
+// void printList(Node* head){
+//     Node* temp = head;
+//     while(temp != null){
+//         cout<<temp->data<<" ";
+//         temp = temp->next;
+//     }
+//     cout<<"\n";
+// }
+// void countOcc(Node* head, int key){
+//     Node* temp = head;
+//     int count = 0;
+//     while(temp != null){
+//         if(temp->data == key) count++;
+//         temp = temp->next;
+//     }
+//     cout<<count<<"\n";
+// }
+// int main(){
+//     Node* head = null;
+//     pushAtEnd(&head, 2);
+//     pushAtEnd(&head, 1);
+//     pushAtEnd(&head, 3);
+//     pushAtEnd(&head, -4);
+//     pushAtEnd(&head, 5);
+//     pushAtEnd(&head, 3);
+//     pushAtEnd(&head, -6);
+//     printList(head);
+//     // counting no. of occurences of a key :
+//     countOcc(head, 3);
+// }
+
+// Practise : Detect a loop in a singly linked list
 #include<bits/stdc++.h>
 #define null NULL
 using namespace std;
@@ -786,25 +838,30 @@ void printList(Node* head){
     }
     cout<<"\n";
 }
-void countOcc(Node* head, int key){
-    Node* temp = head;
-    int count = 0;
-    while(temp != null){
-        if(temp->data == key) count++;
-        temp = temp->next;
-    }
-    cout<<count<<"\n";
-}
 int main(){
     Node* head = null;
-    pushAtEnd(&head, 2);
     pushAtEnd(&head, 1);
+    pushAtEnd(&head, 2);
     pushAtEnd(&head, 3);
-    pushAtEnd(&head, -4);
+    pushAtEnd(&head, 4);
     pushAtEnd(&head, 5);
-    pushAtEnd(&head, 3);
-    pushAtEnd(&head, -6);
-    printList(head);
-    // counting no. of occurences of a key :
-    countOcc(head, 3);
+    pushAtEnd(&head, 6);
+    Node* temp = head; Node* temp1 = head;
+    temp = temp->next->next;
+    while(temp1->next != null) temp1 = temp1->next;
+    temp1->next = temp; // loop made here
+    map<Node*, int> m; int fg = 0;
+    temp = head;
+    for(int i=0; i<6; i++){
+        m[temp->next]++;
+        temp = temp->next;
+    }
+    for(auto i=m.begin(); i!=m.end(); i++){
+        if(i->second > 1){
+            fg = 1;
+            break;
+        }
+    }
+    if(fg == 1) cout<<"Loop found"<<"\n";
+    else cout<<"Loop not found"<<"\n";
 }
