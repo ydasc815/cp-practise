a73fb1c6c67562c3e88b4ab4e06a9a201cdafd74
Aditya <icgadi99@gmail.com>
2020-11-09 14:44:16 +0530

    pre order traversal in BST

--- bst.cpp
+++ bst.cpp
@@ -70,6 +70,58 @@
 
 // Operations in Binary Search Tree
 // level order traversal -> breadth first search - implement using Queue
+// #include<bits/stdc++.h>
+// #define FastIO ios::sync_with_stdio(false);cin.tie(nullptr);
+// #define null NULL
+// using namespace std;
+// class Node{
+//     public:
+//     int data;
+//     Node* left = null;
+//     Node* right = null;
+// };
+// void insertInBST(Node** root, int data){
+//     Node* newNode = new Node();
+//     newNode->data = data;
+//     newNode->left = newNode->right = null;
+//     if(*root == null){
+//         *root = newNode;
+//     } else if(data <= (*root)->data){
+//         insertInBST(&(*root)->left, data);
+//     } else{
+//         insertInBST(&(*root)->right, data);
+//     }
+// }
+// void levelOrderBst(Node* root){
+//     if(root == null) return;
+//     queue<Node*> Q;
+//     Q.push(root);
+//     while(!Q.empty()){
+//         Node* curr = Q.front();
+//         cout<<curr->data<<" ";
+//         if(curr->left != null) Q.push(curr->left);
+//         if(curr->right != null) Q.push(curr->right);
+//         Q.pop();
+//     }
+// }
+// int main(){
+//     FastIO
+//     Node* root = null;
+//     insertInBST(&root, 1);
+//     insertInBST(&root, 3);
+//     insertInBST(&root, 2);
+//     insertInBST(&root, 5);
+//     insertInBST(&root, -1);
+//     insertInBST(&root, 6);
+//     insertInBST(&root, 11);
+//     insertInBST(&root, 10);
+//     insertInBST(&root, 7);
+//     // level order traversal of bst
+//     // perform level order traversal of bst formed from arr = [1, 3, 2, 5, -1, 6, 11, 10, 7]
+//     levelOrderBst(root);
+// }
+
+// Depth first traversals : Preorder, Inorder, Postorder
 #include<bits/stdc++.h>
 #define FastIO ios::sync_with_stdio(false);cin.tie(nullptr);
 #define null NULL
@@ -92,17 +144,11 @@ void insertInBST(Node** root, int data){
         insertInBST(&(*root)->right, data);
     }
 }
-void levelOrderBst(Node* root){
+void preOrderBst(Node* root){
     if(root == null) return;
-    queue<Node*> Q;
-    Q.push(root);
-    while(!Q.empty()){
-        Node* curr = Q.front();
-        cout<<curr->data<<" ";
-        if(curr->left != null) Q.push(curr->left);
-        if(curr->right != null) Q.push(curr->right);
-        Q.pop();
-    }
+    cout<<root->data<<" ";
+    preOrderBst(root->left);
+    preOrderBst(root->right);
 }
 int main(){
     FastIO
@@ -116,7 +162,6 @@ int main(){
     insertInBST(&root, 11);
     insertInBST(&root, 10);
     insertInBST(&root, 7);
-    // level order traversal of bst
-    // perform level order traversal of bst formed from arr = [1, 3, 2, 5, -1, 6, 11, 10, 7]
-    levelOrderBst(root);
+    // perform pre-order traversal of BST formed from arr = [1, 3, 2, 5, -1, 6, 11, 10, 7]
+    preOrderBst(root);
 }
