0165902a5b3d26fa22da19cbe0dc8f0021af61bd
Aditya <icgadi99@gmail.com>
2020-11-13 17:15:37 +0530

    cf-155a

--- cp_prac.cpp
+++ cp_prac.cpp
@@ -14009,5 +14009,18 @@
 using namespace std;
 int main(){
     FastIO
-    
+    int n, cnt = 0; cin>>n; int a[n];
+    for(int i=0; i<n; i++) cin>>a[i];
+    int mx = a[0], mn = a[0];
+    for(int i=1; i<n; i++){
+        if(a[i] > mx) {
+            cnt++;
+            mx = a[i];
+        }
+        if(a[i] < mn) {
+            cnt++;
+            mn = a[i];
+        }
+    }
+    cout<<cnt;
 }
