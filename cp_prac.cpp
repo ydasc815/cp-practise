55cecbadb322b76f83c69dfb0a8e267b7f8476fc
Aditya <icgadi99@gmail.com>
2020-11-21 20:42:16 +0530

    solved tasks

--- cp_prac.cpp
+++ cp_prac.cpp
@@ -14074,3 +14074,20 @@
 // }
 
 // fixing bugs in payo app
+
+// // Aditya @ydasc815
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main() {
+//     int n, x; cin>>n;
+//     vector<int> v;
+//     for(int i=0; i<n; i++){
+//         cin>>x;
+//         if(x == 1) v.push_back(i);
+//     } x = 0;
+//     for(int i=1; i<v.size(); i++){
+//         x = max(x, v[i] - v[i-1] + 1);
+//     }
+//     cout<<x;
+// }
+
